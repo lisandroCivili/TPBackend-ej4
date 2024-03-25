fe3a3df05ee16bca2ca6b60220d2e7c819b6548c
Lisandro Civili <lisandrocivili13@gmail.com>
2024-03-24 21:47:14 -0300

    preparando puertos

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hola')
+import express from 'express';
+import 'dotenv/config' 
+
+
+const app = express() 
+app.set('port', process.env.PORT || 4010)
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+ app.get('port'))
+})
